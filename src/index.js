b43f19c89a63f95da77c2d9b25044e101ad6c77f
Bamieh <ahmadbamieh@gmail.com>
2017-10-15 14:33:52 +0300

    analyze mode and init configs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 'use strict'
 import _ from 'lodash';
 import { evaluateFlow, evaluateSubflow } from './evaluate';
+import analyzeTree from './analyze';
 
 
+const defaultConfig = {
+  watchMode: false,
+  analyzeMode: false,
+};
+
+const userConfig = {};
 const subflows = {};
 
 const reflow = function(name, configCb) {
-  return evaluateFlow(name, configCb);
+  const result = evaluateFlow(name, configCb);
+  if(userConfig.analyzeMode) {
+    const analysisTree = analyzeTree(name, result);
+    console.log(analysisTree.join('\n'))
+  }
+  return result;
 }
 
 Object.assign(reflow, {
+  init(config) {
+    if (config.watchMode) console.log('Running in watch mode.');
+    if (config.analyzeMode) console.log('Running in Analyze mode.');
+
+    return Object.assign(userConfig, defaultConfig, config);
+  },
+  analyzeTree,
   getSuite(name) {
     return {
       name,
