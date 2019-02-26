449236a389e09136c96873600301551563ae38d0
Zainab Salameh <zainabsalameh93@gmail.com>
2019-02-26 14:08:28 +0200

    use webdriver Options

--- packages/reflow-client/src/index.ts
+++ packages/reflow-client/src/index.ts
@@ -3,6 +3,10 @@ import {
 } from 'webdriverio';
 
 import {
+  Options,
+} from 'webdriver';
+
+import {
   reflowClient,
   ClientConfig,
 } from './reflow-client'
@@ -11,36 +15,8 @@ export {
   ClientConfig,
 }
 
-interface DesiredCapabilities {}
-
 export
-interface RemoteOptions {
-  capabilities: DesiredCapabilities,
-  logLevel?: any,
-  logOutput?: any,
-  protocol?: any,
-  host?: any,
-  hostname?: any,
-  port?: any,
-  path?: any,
-  agent?: any,
-  proxy?: any,
-  baseUrl?: any,
-  connectionRetryTimeout?: any,
-  connectionRetryCount?: any,
-  coloredLogs?: any,
-  deprecationWarnings?: any,
-  bail?: any,
-  screenshotPath?: any,
-  screenshotOnReject?: any,
-  waitforTimeout?: any,
-  waitforInterval?: any,
-  queryParams?: any,
-  headers?: any,
-}
-
-export
-function createClient(remoteOptions: RemoteOptions, clientConfig: ClientConfig) {
+function createClient(remoteOptions :Options, clientConfig: ClientConfig) {
   const client = reflowClient(clientConfig);
   return remote(remoteOptions, client);
 }
